77bbbcded164380a71bb3bf0741e1cf879c9d554
Rachelle Rogers <rachellealaine@wireless-10-146-103-65.public.utexas.edu>
2016-10-18 17:08:32 -0500

    Added city bg

---
+++ iOS-Project/iOS-Project/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  iOS-Project
+//
+//  Created by Justin Tran on 9/29/16.
+//  Copyright © 2016 cs378. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .aspectFill
+        
+        skView.presentScene(scene)
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
